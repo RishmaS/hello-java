a60f6fc6ab2889236e89e26a8d503b7fd373852a
RishmaS <84873271+RishmaS@users.noreply.github.com>
2021-05-26 15:09:00 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,22 @@
+/************************************
+*Java Program Name : Hello Java
+*************************************
+*Rishma Singh
+*May 26, 2021
+*CMSC 255 C01 SU2021
+*************************************/
+
+public class Hello {
+	public static void main(String[] args) {
+	
+		System.out.println("Rishma Singh");
+		System.out.println("Hello Java");
+		System.out.println("Java Progam Lab 01");
+		System.out.println("May 26, 2021");
+		System.out.println("CMSC 255 C01 SU2021");
+		System.out.println();
+	
+	}
+
+
+}
